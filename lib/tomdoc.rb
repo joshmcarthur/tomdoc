aa908de089929e19d0e76d0e4d537357bbaa732e
Chris Wanstrath <chris@ozmm.org>
2010-04-06 21:23:48 -0700

    console generator

--- lib/tomdoc.rb
+++ lib/tomdoc.rb
@@ -1,16 +1,21 @@
-require 'open3'
 require 'pp'
 
 require 'ruby_parser'
 require 'colored'
 
-require 'tomdoc/method'
-require 'tomdoc/scope'
-require 'tomdoc/parser'
-
-require 'tomdoc/generator'
-
 module TomDoc
+  autoload :Open3,     'open3'
+
+  autoload :Method,    'tomdoc/method'
+  autoload :Scope,     'tomdoc/scope'
+  autoload :Parser,    'tomdoc/parser'
+  autoload :Generator, 'tomdoc/generator'
+
+  module Generators
+    autoload :Console, 'tomdoc/generators/console'
+    autoload :HTML,    'tomdoc/generators/html'
+  end
+
   def self.generate(text)
     Generator.process(Parser.parse(text))
   end
--- lib/tomdoc/generator.rb
+++ lib/tomdoc/generator.rb
@@ -1,10 +1,14 @@
 module TomDoc
   class Generator
+    def self.generate(text)
+      process Parser.parse(text)
+    end
+
     def self.process(sexp)
       new(sexp).process
     end
 
-    attr_reader :scopes, :classes
+    attr_reader :scopes
     def initialize(scopes)
       @scopes = scopes
     end
@@ -12,87 +16,50 @@ module TomDoc
     def process(scopes = @scopes, prefix = nil)
       scopes.each do |name, scope|
         if scope.is_a? Scope
-          class_methods(scope, prefix)
-          instance_methods(scope, prefix)
+          write_scope(scope, prefix)
         else
           process(scope, "#{name}::")
         end
       end
     end
 
-    def class_methods(scope, prefix = nil)
+    def write_scope(scope, prefix)
+      write_scope_header(scope, prefix)
+      write_class_methods(scope, prefix)
+      write_instance_methods(scope, prefix)
+      write_scope_footer(scope, prefix)
+    end
+
+    def write_scope_header(scope, prefix)
+    end
+
+    def write_scope_footer(scope, prefix)
+    end
+
+    def write_class_methods(scope, prefix = nil)
       scope.class_methods.each do |method|
-        print_method(method, "#{prefix}#{scope.name}.")
+        write_method(method, "#{prefix}#{scope.name}.")
       end
     end
 
-    def instance_methods(scope, prefix = nil)
+    def write_instance_methods(scope, prefix = nil)
       scope.instance_methods.each do |method|
-        print_method(method, "#{prefix}#{scope.name}#")
+        write_method(method, "#{prefix}#{scope.name}#")
       end
     end
 
-    def print_method(method, prefix = '')
-      puts '-' * 80
-      signature = "#{prefix}#{method.name}#{args(method)}"
-
-      puts signature.bold, ''
-      puts format_comment(method.tomdoc)
+    def write_method(method, prefix = '')
     end
 
-    def args(method)
-      if method.args.any?
-        '(' + method.args.join(', ') + ')'
-      else
-        ''
-      end
-    end
-
-    def format_comment(comment)
-      # Strip leading comments
-      comment.gsub!(/^# ?/, '')
-
-      # Example code
-      comment.gsub!(/(\s*Examples\s*(.+?)\s*Returns)/m) do
-        $1.sub($2, highlight($2))
-      end
-
-      # Param list
-      comment.gsub!(/^(\s*(\w+) +- )/) do
-        param = $2
-        $1.sub(param, param.green)
-      end
-
-      # true/false/nil
-      comment.gsub!(/(true|false|nil)/, '\1'.magenta)
-
-      # Strings
-      comment.gsub!(/('.+?')/, '\1'.yellow)
-      comment.gsub!(/(".+?")/, '\1'.yellow)
-
-      # Symbols
-      comment.gsub!(/(\s+:\w+)/, '\1'.red)
-
-      # Constants
-      comment.gsub!(/(([A-Z]\w+(::)?)+)/) do
-        if constant?($1.strip)
-          $1.split('::').map { |part| part.cyan }.join('::')
-        else
-          $1
-        end
-      end
-
-      comment
-    end
-
-    def highlight(text)
-      command = "pygmentize -l ruby"
+    def pygments(text, *args)
       out = ''
-      Open3.popen3(command) do |stdin, stdout, stderr|
+
+      Open3.popen3("pygmentize", *args) do |stdin, stdout, stderr|
         stdin.puts text
         stdin.close
         out = stdout.read.chomp
       end
+
       out
     end
 
