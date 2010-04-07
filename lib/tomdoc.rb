60892e945df02544ac5e70e3c745dd541b2923b3
Chris Wanstrath <chris@ozmm.org>
2010-04-06 17:21:45 -0700

    fix requires

--- lib/tomdoc.rb
+++ lib/tomdoc.rb
@@ -4,11 +4,11 @@ require 'open3'
 require 'ruby_parser'
 require 'colored'
 
+require 'tomdoc/method'
+require 'tomdoc/scope'
 require 'tomdoc/parser'
-require 'tomdoc/generator'
 
-require 'tomdoc/generators/html'
-require 'tomdoc/generators/console'
+require 'tomdoc/generator'
 
 module TomDoc
   def self.generate(text)
