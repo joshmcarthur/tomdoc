0e8efc2394c9081965924c291f1ebf3c22d5f0f8
Chris Wanstrath <chris@ozmm.org>
2010-04-08 00:58:15 -0700

    Fully functional CLI

--- lib/tomdoc/cli.rb
+++ lib/tomdoc/cli.rb
@@ -33,34 +33,43 @@ module TomDoc
       separator "Options:"
 
       on "-c", "--colored", "Pass -p, -s, or -t output to Pygments." do
+        ARGV.delete('-c') || ARGV.delete('--colored')
         exec "#{$0} #{ARGV * ' '} | pygmentize -l ruby"
       end
 
       on "-t", "--tokens FILE", "Parse FILE and print the tokenized form." do
         parser = SourceParser.new.parser
-        sexp   = parser.parse(ARGF.read)
+        sexp   = parser.parse(argf.read)
 
         pp SourceParser.new.tokenize(sexp)
+        exit
       end
 
       on "-s", "--sexp FILE", "Parse FILE and print the AST's sexps." do
-        pp RubyParser.new.parse(ARGF.read).to_a
+        pp RubyParser.new.parse(argf.read).to_a
+        exit
       end
 
       on "-w", "--html FILE", "Parse FILE and print the TomDoc as HTML." do
-        puts Generators::HTML.generate(ARGF.read)
+        puts Generators::HTML.generate(argf.read)
+        exit
       end
 
       separator " "
       separator "Common Options:"
 
-      on "-v", "--version", "Print the version" do |v|
+      on "-v", "--version", "Print the version" do
         puts "TomDoc v#{VERSION}"
         exit
       end
 
-      on_tail("-h", "--help", "Show this message") do
+      on "-h", "--help", "Show this message" do
         puts self
+        exit
+      end
+
+      on_tail do
+        puts Generators::Console.generate(argf.read)
         exit
       end
 
@@ -72,21 +81,27 @@ module TomDoc
     # Actions
     #
 
-    def self.process_files(stream)
-      new.process_files(stream)
-    end
-
-    def process_files(stream)
-      puts TomDoc::Generators::Console.generate(stream.read)
-    end
-
     def self.parse_options(args)
       new.parse_options(args)
     end
 
     def parse_options(args)
-      options.parse!(args)
+      options.parse(args)
     end
+  end
+end
+
+class OptionParser
+  # ARGF faker.
+  def argf
+    buffer = ''
+
+    ARGV.select { |arg| File.exists?(arg) }.each do |file|
+      buffer << File.read(file)
+    end
+
+    require 'stringio'
+    StringIO.new(buffer)
   end
 end
 
